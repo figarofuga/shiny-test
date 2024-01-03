4dde10e19e4ed2bba5e783278ece05dc92bfa6e3
figarofuga <61215157+figarofuga@users.noreply.github.com>
2024-01-03 12:31:27 +0000

    start readtable first

--- make-tableone/app.R
+++ make-tableone/app.R
@@ -21,16 +21,14 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            fileInput(
+                "file1",
+                "choose CSV File",
+                accept = ".csv"), 
+            checkboxInput("header", "Header", TRUE)
         ),
-
-        # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+            tableOutput("contents")
         )
     )
 )
@@ -38,15 +36,16 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
+    output$contents <- renderTable({
         # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        file    <- input$file1
+        ext <- tools::file_ext(file$datapath)
+
+        req(file)
+        validate(need(ext == "csv", "Please upload a csv file"))
 
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+        rio::import(file$datapath, heaer = input$header) 
     })
 }
 
